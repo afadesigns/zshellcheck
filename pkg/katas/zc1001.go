812ac149d3580824ad663c8019dccb25ddcaf0bf
afadesigns <afadesign.official@gmail.com>
2025-11-12 23:34:24 +0100

    feat: Reintroduce full ZC1001 Kata logic to detect [ in if conditions

--- pkg/katas/zc1001.go
+++ pkg/katas/zc1001.go
@@ -2,6 +2,7 @@ package katas
 
 import (
 	"github.com/afadesigns/zshellcheck/pkg/ast"
+	"github.com/afadesigns/zshellcheck/pkg/token"
 )
 
 func init() {
@@ -15,5 +16,20 @@ func init() {
 
 func checkZC1001(node ast.Node) []Violation {
 	violations := []Violation{}
+
+	ifExpression, ok := node.(*ast.IfExpression); ok {
+		// We are looking for an IfExpression where the condition is a BracketExpression
+		// This indicates the use of the old '[' command.
+		if bracketExp, ok := ifExpression.Condition.(*ast.BracketExpression); ok {
+			// The BracketExpression itself holds the token for '['
+			violations = append(violations, Violation{
+				KataID:  "ZC1001",
+				Message: "Prefer [[ over [ for Zsh-specific tests. [[ is a Zsh keyword, offering safer and more powerful conditional expressions.",
+				Line:    bracketExp.Token.Line,
+				Column:  bracketExp.Token.Column,
+			})
+		}
+	}
+
 	return violations
 }
