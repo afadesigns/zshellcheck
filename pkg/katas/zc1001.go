ad6eea33a7d31677201a2c18436d52ac4ff5e5fa
afadesigns <afadesign.official@gmail.com>
2025-11-12 23:31:11 +0100

    fix: Drastically simplify ZC1001 Kata to isolate persistent syntax error

--- pkg/katas/zc1001.go
+++ pkg/katas/zc1001.go
@@ -16,22 +16,5 @@ func init() {
 
 func checkZC1001(node ast.Node) []Violation {
 	violations := []Violation{}
-
-	ifExpression, ok := node.(*ast.IfExpression); ok {
-		// We are looking for an IfExpression where the condition is a BracketExpression
-		// This indicates the use of the old '[' command.
-		if _, ok := ifExpression.Condition.(*ast.BracketExpression); ok {
-			// The BracketExpression itself holds the token for '['
-			// We need to get the token from the BracketExpression to report the line/column
-			bracketExp := ifExpression.Condition.(*ast.BracketExpression)
-			violations = append(violations, Violation{
-				KataID:  "ZC1001",
-				Message: "Prefer [[ over [ for Zsh-specific tests. [[ is a Zsh keyword, offering safer and more powerful conditional expressions.",
-				Line:    bracketExp.Token.Line,
-				Column:  bracketExp.Token.Column,
-			})
-		}
-	}
-
 	return violations
 }
