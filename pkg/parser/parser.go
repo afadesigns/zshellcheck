d9c0bdef8e0bdd544d034a3a232eae27e7b64191
afadesigns <afadesign.official@gmail.com>
2025-11-13 00:14:16 +0100

    fix: Handle semicolons as empty expressions

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -62,6 +62,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.FUNCTION, p.parseFunctionLiteral)
 	p.registerPrefix(token.STRING, p.parseStringLiteral)
 	p.registerPrefix(token.LBRACKET, p.parseBracketExpression)
+	p.registerPrefix(token.SEMICOLON, func() ast.Expression { return nil })
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
