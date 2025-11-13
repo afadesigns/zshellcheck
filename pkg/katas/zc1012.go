781e736d5d6470a79c891c742341cc37d0d4357e
afadesigns <afadesign.official@gmail.com>
2025-11-13 02:39:10 +0100

    feat(kata): add zc1012 - prefer $(...) over backticks

---
+++ pkg/katas/zc1012.go
@@ -0,0 +1,31 @@
+package katas
+
+import (
+	"reflect"
+
+	"github.com/afadesigns/zshellcheck/pkg/ast"
+)
+
+func init() {
+	RegisterKata(reflect.TypeOf(&ast.CommandSubstitution{}), Kata{
+		ID:          "ZC1012",
+		Title:       "Use `$(command)` instead of backticks for command substitution",
+		Description: "The `$(command)` syntax is generally preferred over backticks `` `command` `` for command substitution. It's easier to read, can be nested, and avoids issues with backslashes.",
+		Check:       checkZC1012,
+	})
+}
+
+func checkZC1012(node ast.Node) []Violation {
+	violations := []Violation{}
+
+	if cmdSub, ok := node.(*ast.CommandSubstitution); ok {
+		violations = append(violations, Violation{
+			KataID:  "ZC1012",
+			Message: "Use `$(command)` instead of backticks for command substitution.",
+			Line:    cmdSub.Token.Line,
+			Column:  cmdSub.Token.Column,
+		})
+	}
+
+	return violations
+}
