1f1fcd09490da5aefbfb8b6d527e43b4f2fde80e
afadesigns <afadesign.official@gmail.com>
2025-11-13 17:09:25 +0100

    feat(kata): add zc1017 to recommend print -r

---
+++ pkg/katas/zc1017.go
@@ -0,0 +1,42 @@
+package katas
+
+import (
+	"reflect"
+
+	"github.com/afadesigns/zshellcheck/pkg/ast"
+)
+
+func init() {
+	RegisterKata(reflect.TypeOf(&ast.SimpleCommand{}), Kata{
+		ID:          "ZC1017",
+		Title:       "Use `print -r` to print strings literally",
+		Description: "The `print` command interprets backslash escape sequences by default. To print a string literally, use the `-r` option.",
+		Check:       checkZC1017,
+	})
+}
+
+func checkZC1017(node ast.Node) []Violation {
+	violations := []Violation{}
+
+	if cmd, ok := node.(*ast.SimpleCommand); ok {
+		if name, ok := cmd.Name.(*ast.Identifier); ok && name.Value == "print" {
+			hasRFlag := false
+			for _, arg := range cmd.Arguments {
+				if ident, ok := arg.(*ast.Identifier); ok && ident.Value == "-r" {
+					hasRFlag = true
+					break
+				}
+			}
+			if !hasRFlag {
+				violations = append(violations, Violation{
+					KataID:  "ZC1017",
+					Message: "Use `print -r` to print strings literally.",
+					Line:    name.Token.Line,
+					Column:  name.Token.Column,
+				})
+			}
+		}
+	}
+
+	return violations
+}
