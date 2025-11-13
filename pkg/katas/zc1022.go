0d75004c5217845019e9c17839e29f7b65723d65
afadesigns <afadesign.official@gmail.com>
2025-11-13 17:16:18 +0100

    feat(kata): add zc1022 to prefer arithmetic expansion over let

---
+++ pkg/katas/zc1022.go
@@ -0,0 +1,31 @@
+package katas
+
+import (
+	"reflect"
+
+	"github.com/afadesigns/zshellcheck/pkg/ast"
+)
+
+func init() {
+	RegisterKata(reflect.TypeOf(&ast.LetStatement{}), Kata{
+		ID:          "ZC1022",
+		Title:       "Use `$((...))` for arithmetic expansion",
+		Description: "The `$((...))` syntax is the modern, recommended way to perform arithmetic expansion. It is more readable and can be nested easily, unlike `let`.",
+		Check:       checkZC1022,
+	})
+}
+
+func checkZC1022(node ast.Node) []Violation {
+	violations := []Violation{}
+
+	if let, ok := node.(*ast.LetStatement); ok {
+		violations = append(violations, Violation{
+			KataID:  "ZC1022",
+			Message: "Use `$((...))` for arithmetic expansion instead of `let`.",
+			Line:    let.Token.Line,
+			Column:  let.Token.Column,
+		})
+	}
+
+	return violations
+}
