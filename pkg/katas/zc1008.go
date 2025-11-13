40d7f3b22977219b84f8595a4706741fdcf0c2ff
afadesigns <afadesign.official@gmail.com>
2025-11-13 02:34:30 +0100

    feat(kata): add zc1008 - prefer arithmetic expansion over let

---
+++ pkg/katas/zc1008.go
@@ -0,0 +1,31 @@
+package katas
+
+import (
+	"reflect"
+
+	"github.com/afadesigns/zshellcheck/pkg/ast"
+)
+
+func init() {
+	RegisterKata(reflect.TypeOf(&ast.LetStatement{}), Kata{
+		ID:          "ZC1008",
+		Title:       "Use `\\$(())` for arithmetic operations",
+		Description: "The `let` command is a shell builtin, but the `\\$(())` syntax is more portable and generally preferred for arithmetic operations in Zsh. It's also more powerful as it can be used in more contexts.",
+		Check:       checkZC1008,
+	})
+}
+
+func checkZC1008(node ast.Node) []Violation {
+	violations := []Violation{}
+
+	if letStmt, ok := node.(*ast.LetStatement); ok {
+		violations = append(violations, Violation{
+			KataID:  "ZC1008",
+			Message: "Use `\\$(())` for arithmetic operations instead of `let`.",
+			Line:    letStmt.Token.Line,
+			Column:  letStmt.Token.Column,
+		})
+	}
+
+	return violations
+}
