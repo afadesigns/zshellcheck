f8945ca4dae6f7c71c37d37ca5e467242a1d7203
afadesigns <afadesign.official@gmail.com>
2025-11-13 20:30:46 +0100

    feat(kata): add zc1032 to prefer (( i++ )) over let i=i+1

---
+++ pkg/katas/zc1032.go
@@ -0,0 +1,39 @@
+package katas
+
+import (
+	"reflect"
+
+	"github.com/afadesigns/zshellcheck/pkg/ast"
+)
+
+func init() {
+	RegisterKata(reflect.TypeOf(&ast.LetStatement{}), Kata{
+		ID:          "ZC1032",
+		Title:       "Use `((...))` for C-style incrementing",
+		Description: "Instead of `let i=i+1`, you can use the more concise and idiomatic C-style increment `(( i++ ))` in Zsh.",
+		Check:       checkZC1032,
+	})
+}
+
+func checkZC1032(node ast.Node) []Violation {
+	violations := []Violation{}
+
+	if letStmt, ok := node.(*ast.LetStatement); ok {
+		if infixExpr, ok := letStmt.Value.(*ast.InfixExpression); ok {
+			if leftIdent, ok := infixExpr.Left.(*ast.Identifier); ok {
+				if rightInt, ok := infixExpr.Right.(*ast.IntegerLiteral); ok {
+					if letStmt.Name.Value == leftIdent.Value && infixExpr.Operator == "+" && rightInt.Value == 1 {
+						violations = append(violations, Violation{
+							KataID:  "ZC1032",
+							Message: "Use `(( i++ ))` for C-style incrementing instead of `let i=i+1`.",
+							Line:    letStmt.Token.Line,
+							Column:  letStmt.Token.Column,
+						})
+					}
+				}
+			}
+		}
+	}
+
+	return violations
+}
