213c5d4864db80a1ec6dcb3565cdcd10b9975356
afadesigns <afadesign.official@gmail.com>
2025-11-13 17:06:07 +0100

    feat(kata): add zc1014 to prefer git switch/restore over checkout

---
+++ pkg/katas/zc1014.go
@@ -0,0 +1,37 @@
+package katas
+
+import (
+	"reflect"
+
+	"github.com/afadesigns/zshellcheck/pkg/ast"
+)
+
+func init() {
+	RegisterKata(reflect.TypeOf(&ast.SimpleCommand{}), Kata{
+		ID:          "ZC1014",
+		Title:       "Use `git switch` or `git restore` instead of `git checkout`",
+		Description: "The `git checkout` command can be ambiguous. `git switch` is used for switching branches and `git restore` is used for restoring files. Using these more specific commands can make your scripts clearer and less error-prone.",
+		Check:       checkZC1014,
+	})
+}
+
+func checkZC1014(node ast.Node) []Violation {
+	violations := []Violation{}
+
+	if cmd, ok := node.(*ast.SimpleCommand); ok {
+		if name, ok := cmd.Name.(*ast.Identifier); ok && name.Value == "git" {
+			if len(cmd.Arguments) > 0 {
+				if arg, ok := cmd.Arguments[0].(*ast.Identifier); ok && arg.Value == "checkout" {
+					violations = append(violations, Violation{
+						KataID:  "ZC1014",
+						Message: "Use `git switch` or `git restore` instead of the ambiguous `git checkout`.",
+						Line:    name.Token.Line,
+						Column:  name.Token.Column,
+					})
+				}
+			}
+		}
+	}
+
+	return violations
+}
