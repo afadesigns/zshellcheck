2dbdb69b8064becdd59bcccfb19014a2253b65f3
afadesigns <afadesign.official@gmail.com>
2025-11-13 17:10:19 +0100

    feat(kata): add zc1018 to prefer ((...)) over expr

---
+++ pkg/katas/zc1018.go
@@ -0,0 +1,35 @@
+package katas
+
+import (
+	"reflect"
+
+	"github.com/afadesigns/zshellcheck/pkg/ast"
+)
+
+func init() {
+	RegisterKata(reflect.TypeOf(&ast.SimpleCommand{}), Kata{
+		ID:          "ZC1018",
+		Title:       "Use `((...))` for C-style arithmetic instead of `expr`",
+		Description: "The `((...))` construct in Zsh allows for C-style arithmetic. It is generally more efficient and readable than using `expr` or other external commands for arithmetic.",
+		Check:       checkZC1018,
+	})
+}
+
+func checkZC1018(node ast.Node) []Violation {
+	violations := []Violation{}
+
+	if cmd, ok := node.(*ast.SimpleCommand); ok {
+		if ident, ok := cmd.Name.(*ast.Identifier); ok {
+			if ident.Value == "expr" {
+				violations = append(violations, Violation{
+					KataID:  "ZC1018",
+					Message: "Use `((...))` for C-style arithmetic instead of `expr`.",
+					Line:    ident.Token.Line,
+					Column:  ident.Token.Column,
+				})
+			}
+		}
+	}
+
+	return violations
+}
