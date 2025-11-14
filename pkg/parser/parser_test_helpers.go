4054e2db3bfd523b2273f2df0b6aa95b1f2d9f27
afadesigns <afadesign.official@gmail.com>
2025-11-14 03:05:03 +0100

    refactor(parser): move checkParserErrors to helper file

---
+++ pkg/parser/parser_test_helpers.go
@@ -0,0 +1,20 @@
+package parser
+
+import (
+	"testing"
+
+	"github.com/afadesigns/zshellcheck/pkg/lexer"
+)
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
+}
