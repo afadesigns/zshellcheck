22898692531b5782aa52284f30e3fe2eabd71256
afadesigns <afadesign.official@gmail.com>
2026-03-30 22:30:00 +0200

    fix: Remove overly aggressive katas breaking integration tests
    Remove ZC1130 (true->:), ZC1138 (printf '%s\n'), ZC1150 (cat file)
    as they trigger false positives on common valid patterns in
    integration tests. Version adjusted to 0.1.47 (147 katas).

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 150 Katas = 0.1.50
-const Version = "0.1.50"
+// 147 Katas = 0.1.47
+const Version = "0.1.47"
