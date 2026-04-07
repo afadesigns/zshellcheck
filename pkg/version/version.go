f85704f3f574880d634cb3ccb215a74d4f291a95
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-07 03:54:19 +0200

    feat: Add ZC1280 — use Zsh ${var:e} instead of cut for file extension (#307)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 276 Katas = 0.2.76
-const Version = "0.2.76"
+// 277 Katas = 0.2.77
+const Version = "0.2.77"
