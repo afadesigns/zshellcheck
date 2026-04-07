7ef6c681872d446a5712d4db5d584591a9585af6
afadesigns <afadesign.official@gmail.com>
2026-04-07 03:18:06 +0200

    feat: Add ZC1271 — use command -v instead of which

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 267 Katas = 0.2.67
-const Version = "0.2.67"
+// 268 Katas = 0.2.68
+const Version = "0.2.68"
