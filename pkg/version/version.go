7d293bb2edfc3b85a0cde793ab18a681c5b74dc5
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-07 03:50:14 +0200

    feat: Add ZC1279 — use realpath instead of readlink -f (#306)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 275 Katas = 0.2.75
-const Version = "0.2.75"
+// 276 Katas = 0.2.76
+const Version = "0.2.76"
