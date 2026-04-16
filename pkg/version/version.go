d4aeee8ae985465d905d0f83edd8bb7dbd2f47fa
afadesigns <afadesign.official@gmail.com>
2026-04-17 01:41:45 +0200

    feat: ZC1339 — use `${#${(f)var}}` instead of `wc -l` for line count

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 335 Katas = 0.3.35
-const Version = "0.3.35"
+// 336 Katas = 0.3.36
+const Version = "0.3.36"
