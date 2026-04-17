7a45de6c073928d81dfd0309fceb2a7b1e66d517
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-17 04:40:44 +0200

    feat: ZC1372 — use Zsh `zmv` autoload instead of `rename`/`rename.ul` (#385)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 368 Katas = 0.3.68
-const Version = "0.3.68"
+// 369 Katas = 0.3.69
+const Version = "0.3.69"
