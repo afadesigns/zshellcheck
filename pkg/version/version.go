933c1d964c37a125ff1cfd77672b4c3fa02c3cea
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-17 02:33:08 +0200

    feat: ZC1349 — use `${#var}` instead of `expr length` for string length (#362)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 345 Katas = 0.3.45
-const Version = "0.3.45"
+// 346 Katas = 0.3.46
+const Version = "0.3.46"
