2fd6df02bf333b15101f8ef7897660f1024d940f
afadesigns <afadesign.official@gmail.com>
2026-04-17 02:04:18 +0200

    feat: ZC1343 — use Zsh glob age qualifiers instead of `find -mtime`

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 339 Katas = 0.3.39
-const Version = "0.3.39"
+// 340 Katas = 0.3.40
+const Version = "0.3.40"
