a3bae259b7b79d6d403425b63ef746d4afa23f0c
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-17 03:44:20 +0200

    feat: ZC1362 — use `[[ -o option ]]` instead of `test -o option` (#375)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 358 Katas = 0.3.58
-const Version = "0.3.58"
+// 359 Katas = 0.3.59
+const Version = "0.3.59"
