5a315c1c574a8fe2cccbcc2f2cc4008a87ace07c
afadesigns <afadesign.official@gmail.com>
2026-04-17 06:10:02 +0200

    feat: ZC1388 — use Zsh lowercase $mailpath array instead of Bash $MAILPATH

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 384 Katas = 0.3.84
-const Version = "0.3.84"
+// 385 Katas = 0.3.85
+const Version = "0.3.85"
