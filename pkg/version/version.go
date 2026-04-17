bf16a2980d4254a17886105d41f06a500ed67f69
afadesigns <afadesign.official@gmail.com>
2026-04-17 04:07:47 +0200

    feat: ZC1367 — use Zsh `strftime` instead of Bash `printf '%(fmt)T'`

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 363 Katas = 0.3.63
-const Version = "0.3.63"
+// 364 Katas = 0.3.64
+const Version = "0.3.64"
