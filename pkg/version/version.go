cc4ac704b081f04acde39de71d7948d506af652e
afadesigns <afadesign.official@gmail.com>
2026-04-18 00:31:41 +0200

    feat: ZC1595 — flag `setfacl -m u:nobody:... / o::rwx` ACL bypass

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 591 Katas = 0.5.91
-const Version = "0.5.91"
+// 592 Katas = 0.5.92
+const Version = "0.5.92"
