649eb4199b483d80f26183ee65a26d6ba92fd529
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-18 01:35:28 +0200

    feat: ZC1608 — flag `find -exec sh -c '... {} ...'` filename injection (#627)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 604 Katas = 0.6.4
-const Version = "0.6.4"
+// 605 Katas = 0.6.5
+const Version = "0.6.5"
