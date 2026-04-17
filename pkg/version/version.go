e69b55aa8f293adb3061602874791abafa00b3e8
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-18 01:01:18 +0200

    feat: ZC1601 — flag `ethtool -s $IF wol` enabling Wake-on-LAN (#620)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 597 Katas = 0.5.97
-const Version = "0.5.97"
+// 598 Katas = 0.5.98
+const Version = "0.5.98"
