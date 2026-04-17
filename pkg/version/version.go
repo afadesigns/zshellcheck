90fd1cb6e2f9015753892b0beda898d9dd562ca3
afadesigns <afadesign.official@gmail.com>
2026-04-17 08:11:22 +0200

    feat: ZC1408 — warn on $BASH_FUNC_* exported-function envvar

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 404 Katas = 0.4.4
-const Version = "0.4.4"
+// 405 Katas = 0.4.5
+const Version = "0.4.5"
