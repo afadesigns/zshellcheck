9f765eb62359b4e7fb843643f3f0089b315fd8e3
afadesigns <afadesign.official@gmail.com>
2026-04-17 06:42:42 +0200

    feat: ZC1394 — warn on $BASH interpreter var (use Zsh $ZSH_NAME)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 390 Katas = 0.3.90
-const Version = "0.3.90"
+// 391 Katas = 0.3.91
+const Version = "0.3.91"
