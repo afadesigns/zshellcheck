d33f1e9f01b80315be1371a189cda087c8640f22
afadesigns <afadesign.official@gmail.com>
2026-04-18 01:17:42 +0200

    feat: ZC1605 — flag `debugfs -w DEV` write-mode fs debugger bypass

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 601 Katas = 0.6.1
-const Version = "0.6.1"
+// 602 Katas = 0.6.2
+const Version = "0.6.2"
