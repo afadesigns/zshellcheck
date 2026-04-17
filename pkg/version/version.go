692111feaea6d4402a97fc74ed3f906e6a5e9825
afadesigns <afadesign.official@gmail.com>
2026-04-17 09:53:15 +0200

    feat: ZC1425 — warn on shutdown/reboot/halt/poweroff in scripts

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 421 Katas = 0.4.21
-const Version = "0.4.21"
+// 422 Katas = 0.4.22
+const Version = "0.4.22"
