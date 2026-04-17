26073e869a79440180fb2b7a49a8c00ad5306a13
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-17 03:23:20 +0200

    feat: ZC1358 — use `${PWD:P}` instead of `pwd -P` for canonical path (#371)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 354 Katas = 0.3.54
-const Version = "0.3.54"
+// 355 Katas = 0.3.55
+const Version = "0.3.55"
