e86c30acb8e255f819cbbf3ee94d9c425e33decf
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-17 07:00:22 +0200

    feat: ZC1397 — warn on Bash $COMP_TYPE/$COMP_KEY (use Zsh $compstate) (#411)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 393 Katas = 0.3.93
-const Version = "0.3.93"
+// 394 Katas = 0.3.94
+const Version = "0.3.94"
