651149112622352bb6d1adcc835bc1d4a422c18b
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-17 11:04:55 +0200

    feat: ZC1433 — warn on `userdel -f`/`-r` (forcibly removes user + data) (#450)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 429 Katas = 0.4.29
-const Version = "0.4.29"
+// 430 Katas = 0.4.30
+const Version = "0.4.30"
