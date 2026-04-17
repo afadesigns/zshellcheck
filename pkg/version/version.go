5b0106496c0860904b9c8b86d7e01a15527f0c11
afadesigns <afadesign.official@gmail.com>
2026-04-17 03:53:25 +0200

    feat: ZC1364 — use Zsh `${var:pos:len}` instead of `cut -c`

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 360 Katas = 0.3.60
-const Version = "0.3.60"
+// 361 Katas = 0.3.61
+const Version = "0.3.61"
