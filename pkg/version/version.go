87c6d7aa42941c5a572512e1b994a949c2d2ccbe
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-17 06:06:15 +0200

    feat: ZC1387 — use Zsh $options assoc array instead of Bash $SHELLOPTS (#401)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 383 Katas = 0.3.83
-const Version = "0.3.83"
+// 384 Katas = 0.3.84
+const Version = "0.3.84"
