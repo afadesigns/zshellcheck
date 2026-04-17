05551c6ae9d6d08cecfdab9532e940f19a9f9216
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-17 06:22:20 +0200

    feat: ZC1390 — clarify $GROUPS is scalar in Zsh (Bash array syntax breaks) (#404)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 386 Katas = 0.3.86
-const Version = "0.3.86"
+// 387 Katas = 0.3.87
+const Version = "0.3.87"
