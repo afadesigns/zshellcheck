ab3e11cc0a519f9f03b31d73774ec5ad10c0b8eb
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-17 10:51:32 +0200

    feat: ZC1431 — warn on `crontab -r` (deletes all user cron jobs) (#447)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 427 Katas = 0.4.27
-const Version = "0.4.27"
+// 428 Katas = 0.4.28
+const Version = "0.4.28"
