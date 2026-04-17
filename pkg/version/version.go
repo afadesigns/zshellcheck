778ca6ae6088d567888b9765694fcbd5e8f850ea
afadesigns <afadesign.official@gmail.com>
2026-04-18 01:37:39 +0200

    feat: ZC1609 — flag `aa-disable` / `aa-complain` / `apparmor_parser -R`

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 605 Katas = 0.6.5
-const Version = "0.6.5"
+// 606 Katas = 0.6.6
+const Version = "0.6.6"
