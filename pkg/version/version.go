3db80a86e2d4107b3953de93320c91845d192814
afadesigns <afadesign.official@gmail.com>
2026-04-18 01:04:11 +0200

    feat: ZC1602 — flag `setopt KSH_ARRAYS / SH_WORD_SPLIT` global semantics flip

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 598 Katas = 0.5.98
-const Version = "0.5.98"
+// 599 Katas = 0.5.99
+const Version = "0.5.99"
