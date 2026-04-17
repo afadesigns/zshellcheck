19df1a3f082a0656ee6a86f1531643427ec1da34
afadesigns <afadesign.official@gmail.com>
2026-04-17 08:52:36 +0200

    feat: ZC1415 — prefer Zsh TRAPZERR function over `trap 'cmd' ERR`

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 411 Katas = 0.4.11
-const Version = "0.4.11"
+// 412 Katas = 0.4.12
+const Version = "0.4.12"
