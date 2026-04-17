aaac8f1c358935ca8b30088f6f9fac76bb0cc69c
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-17 03:28:35 +0200

    feat: ZC1359 — use Zsh `$groups` assoc array instead of `id -Gn` (#372)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 355 Katas = 0.3.55
-const Version = "0.3.55"
+// 356 Katas = 0.3.56
+const Version = "0.3.56"
