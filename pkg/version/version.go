2a76748178451d6f93a4ebce9dfd480c47e3e6bd
afadesigns <afadesign.official@gmail.com>
2026-04-19 01:20:11 +0200

    feat: ZC1741 — flag `mkpasswd PASSWORD` (cleartext password in argv)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 737 Katas = 0.7.37
-const Version = "0.7.37"
+// 738 Katas = 0.7.38
+const Version = "0.7.38"
