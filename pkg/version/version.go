73a21c9c4f78b61259b03df8986a9cff32a45e09
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-19 00:24:28 +0200

    feat: ZC1733 — flag `docker plugin install --grant-all-permissions` (silent root grant) (#753)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 729 Katas = 0.7.29
-const Version = "0.7.29"
+// 730 Katas = 0.7.30
+const Version = "0.7.30"
