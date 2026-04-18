ead0cbb6ed7a0035aae4287e24111435e6af69af
afadesigns <afadesign.official@gmail.com>
2026-04-18 23:55:24 +0200

    feat: ZC1730 — flag `brew install --HEAD` (unrepeatable upstream-HEAD build)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 726 Katas = 0.7.26
-const Version = "0.7.26"
+// 727 Katas = 0.7.27
+const Version = "0.7.27"
