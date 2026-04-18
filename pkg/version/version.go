7a2ac61f7afd3f55d5a4781f5ca602789a871999
afadesigns <afadesign.official@gmail.com>
2026-04-18 04:32:19 +0200

    feat: ZC1645 — prefer `/etc/os-release` over `lsb_release`

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 641 Katas = 0.6.41
-const Version = "0.6.41"
+// 642 Katas = 0.6.42
+const Version = "0.6.42"
