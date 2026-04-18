b570421f26dd57e40221c64be997215c72c6d534
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-18 03:51:52 +0200

    feat: ZC1636 — flag `virsh destroy DOMAIN` force VM power-off (#655)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 632 Katas = 0.6.32
-const Version = "0.6.32"
+// 633 Katas = 0.6.33
+const Version = "0.6.33"
