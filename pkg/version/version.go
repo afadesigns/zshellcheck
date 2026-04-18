d0280983e3b8176d8c2940511f330aa2b3b1f113
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-18 05:11:23 +0200

    feat: ZC1651 — flag `docker run -p 0.0.0.0:PORT:PORT` all-interfaces publish (#670)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 647 Katas = 0.6.47
-const Version = "0.6.47"
+// 648 Katas = 0.6.48
+const Version = "0.6.48"
