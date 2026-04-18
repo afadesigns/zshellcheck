17da295847deb3701be147b1360c287e028da11c
afadesigns <afadesign.official@gmail.com>
2026-04-18 03:17:01 +0200

    feat: ZC1630 — flag `php -S 0.0.0.0:PORT` dev server on all interfaces

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 626 Katas = 0.6.26
-const Version = "0.6.26"
+// 627 Katas = 0.6.27
+const Version = "0.6.27"
