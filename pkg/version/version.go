aec5593297aca65ee4ad7e4f3f37f8cbdb57c8b8
afadesigns <afadesign.official@gmail.com>
2026-04-19 01:54:56 +0200

    feat: ZC1746 — flag `sysctl kernel.randomize_va_space=0|1` (ASLR weakened)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 742 Katas = 0.7.42
-const Version = "0.7.42"
+// 743 Katas = 0.7.43
+const Version = "0.7.43"
