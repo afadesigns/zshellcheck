e7c7b9b15b9189ffd91db470996aa6ef9f1eff05
afadesigns <afadesign.official@gmail.com>
2026-04-18 06:24:10 +0200

    feat: ZC1664 — flag systemctl set-default rescue/emergency persistent boot

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 660 Katas = 0.6.60
-const Version = "0.6.60"
+// 661 Katas = 0.6.61
+const Version = "0.6.61"
