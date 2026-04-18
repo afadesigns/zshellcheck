5b122e7ec82e4df37e0c289121cf938692d765e2
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-18 08:01:37 +0200

    feat: ZC1679 — flag gcloud add-iam-policy-binding primitive admin roles (#699)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 675 Katas = 0.6.75
-const Version = "0.6.75"
+// 676 Katas = 0.6.76
+const Version = "0.6.76"
