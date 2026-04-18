cb8077012be1f0802eb0395eaac5df96b6eb0829
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-18 19:54:09 +0200

    feat: ZC1701 — flag dpkg -i FILE.deb local install without signature check (#721)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 697 Katas = 0.6.97
-const Version = "0.6.97"
+// 698 Katas = 0.6.98
+const Version = "0.6.98"
