669ba41c92580c78c10d30a04e6005772aefcbc0
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-18 19:29:58 +0200

    feat: ZC1698 — flag fail2ban-client unban --all / stop wipes ban list (#718)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 694 Katas = 0.6.94
-const Version = "0.6.94"
+// 695 Katas = 0.6.95
+const Version = "0.6.95"
