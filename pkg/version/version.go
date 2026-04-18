f25eb0c9d4db921de6ac9a4b66bd77e2fff70e1d
afadesigns <afadesign.official@gmail.com>
2026-04-18 09:41:28 +0200

    feat: ZC1691 — flag rsync --remove-source-files optimistic delete

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 687 Katas = 0.6.87
-const Version = "0.6.87"
+// 688 Katas = 0.6.88
+const Version = "0.6.88"
