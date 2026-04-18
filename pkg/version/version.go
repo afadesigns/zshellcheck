4ef9a49625f62bee09ca607a50421caffbf513de
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-18 20:03:21 +0200

    feat: ZC1702 — flag dpkg-reconfigure without noninteractive frontend (#722)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 698 Katas = 0.6.98
-const Version = "0.6.98"
+// 699 Katas = 0.6.99
+const Version = "0.6.99"
