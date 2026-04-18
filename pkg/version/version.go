4bbb8532b0e3d352858c533ddd05721f25c14b47
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-18 06:31:10 +0200

    feat: ZC1665 — flag chrt -r / -f real-time scheduling in scripts (#685)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 661 Katas = 0.6.61
-const Version = "0.6.61"
+// 662 Katas = 0.6.62
+const Version = "0.6.62"
