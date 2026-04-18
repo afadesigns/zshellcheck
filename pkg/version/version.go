879e6aa32ed09b88798f140468446abea0c93680
afadesigns <afadesign.official@gmail.com>
2026-04-18 04:06:40 +0200

    feat: ZC1640 — prefer Zsh `${(P)var}` over Bash `${!var}` indirect expansion

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 636 Katas = 0.6.36
-const Version = "0.6.36"
+// 637 Katas = 0.6.37
+const Version = "0.6.37"
