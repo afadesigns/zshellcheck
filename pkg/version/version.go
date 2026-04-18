c06b4c773255f9a37854372bfcb8d20c0625bb4f
afadesigns <afadesign.official@gmail.com>
2026-04-18 02:22:20 +0200

    feat: ZC1619 — flag `mount -t nfs/cifs/smb/sshfs` missing `nosuid,nodev`

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 615 Katas = 0.6.15
-const Version = "0.6.15"
+// 616 Katas = 0.6.16
+const Version = "0.6.16"
