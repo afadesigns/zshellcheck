994b9e924b6e65d1a17abecce0107a653d977c89
afadesigns <afadesign.official@gmail.com>
2026-04-18 04:11:31 +0200

    feat: ZC1641 — flag `kubectl create secret --from-literal` / `--docker-password`

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 637 Katas = 0.6.37
-const Version = "0.6.37"
+// 638 Katas = 0.6.38
+const Version = "0.6.38"
