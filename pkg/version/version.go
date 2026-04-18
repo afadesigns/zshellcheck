acfcc898e35200d8e259b9a2ed4a359a786d2f94
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-18 04:19:29 +0200

    feat: ZC1642 — flag `tshark -w` / `dumpcap -w` without `-Z USER` (#661)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 638 Katas = 0.6.38
-const Version = "0.6.38"
+// 639 Katas = 0.6.39
+const Version = "0.6.39"
