060e45e45ae3ecc016303836a4a6e4735c23ad96
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-18 02:35:13 +0200

    feat: ZC1621 — flag `tmux -S /tmp/SOCKET` shared socket hijack risk (#640)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 617 Katas = 0.6.17
-const Version = "0.6.17"
+// 618 Katas = 0.6.18
+const Version = "0.6.18"
