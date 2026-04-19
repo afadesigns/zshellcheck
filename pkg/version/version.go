ec3565cd857afa6481b19c8fda2acf7aba4518e7
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-19 22:03:40 +0200

    feat: ZC1907 — detect `sysctl fs.protected_*=0` disabling /tmp-race safeguards (#928)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 903 Katas = 0.9.3
-const Version = "0.9.3"
+// 904 Katas = 0.9.4
+const Version = "0.9.4"
