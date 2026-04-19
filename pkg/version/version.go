6830fada395626312c44bb03442fad13fcf9fb97
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-19 07:34:19 +0200

    feat: ZC1801 — warn on fwupdmgr update / install flashing firmware (#820)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 797 Katas = 0.7.97
-const Version = "0.7.97"
+// 798 Katas = 0.7.98
+const Version = "0.7.98"
