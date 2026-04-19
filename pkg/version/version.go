8bf6be9389740080613a0a468399b5dc4ea0061c
afadesigns <afadesign.official@gmail.com>
2026-04-19 22:39:25 +0200

    feat: ZC1915 — detect `mdadm --zero-superblock`/`-S` RAID metadata destruction

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 911 Katas = 0.9.11
-const Version = "0.9.11"
+// 912 Katas = 0.9.12
+const Version = "0.9.12"
