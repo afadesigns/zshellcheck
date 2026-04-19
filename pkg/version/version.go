b8e9f0cce45b6fa07847eeac51fa5a6aaa96bb8b
afadesigns <afadesign.official@gmail.com>
2026-04-19 10:59:20 +0200

    feat: ZC1835 — warn on `smartctl -s off` disabling drive SMART monitoring

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 831 Katas = 0.8.31
-const Version = "0.8.31"
+// 832 Katas = 0.8.32
+const Version = "0.8.32"
