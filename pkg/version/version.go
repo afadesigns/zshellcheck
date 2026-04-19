a551361a111b8ad3c7353c65d9ee7e238dae600a
afadesigns <afadesign.official@gmail.com>
2026-04-20 01:23:11 +0200

    feat: ZC1943 — detect `systemd-nspawn -b` booting untrusted rootfs

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 939 Katas = 0.9.39
-const Version = "0.9.39"
+// 940 Katas = 0.9.40
+const Version = "0.9.40"
