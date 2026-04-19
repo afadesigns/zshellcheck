3d7ad55f2e80aa376946b4a2ab5c1cfbfd94a06b
afadesigns <afadesign.official@gmail.com>
2026-04-19 04:58:33 +0200

    feat: ZC1773 — warn on xargs without -r / --no-run-if-empty

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 769 Katas = 0.7.69
-const Version = "0.7.69"
+// 770 Katas = 0.7.70
+const Version = "0.7.70"
