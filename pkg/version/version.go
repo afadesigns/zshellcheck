2cec3ba515ed071821f7014494e9b0ce5671af08
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-19 11:06:29 +0200

    feat: ZC1836 — error on `helm uninstall --no-hooks` skipping cleanup (#857)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 832 Katas = 0.8.32
-const Version = "0.8.32"
+// 833 Katas = 0.8.33
+const Version = "0.8.33"
