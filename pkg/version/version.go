d594b8300344814a80b693352002ac25ce37eb02
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-19 16:52:59 +0200

    feat: ZC1854 — error on `yum-config-manager`/`dnf`/`zypper` adding plaintext HTTP repo (#875)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 850 Katas = 0.8.50
-const Version = "0.8.50"
+// 851 Katas = 0.8.51
+const Version = "0.8.51"
