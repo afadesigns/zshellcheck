3721412ad03e32d5a7326f22a3e52ea6684ed79a
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-19 20:30:09 +0200

    feat: ZC1891 — error on `kubectl config view --raw` printing full kubeconfig with creds (#912)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 887 Katas = 0.8.87
-const Version = "0.8.87"
+// 888 Katas = 0.8.88
+const Version = "0.8.88"
