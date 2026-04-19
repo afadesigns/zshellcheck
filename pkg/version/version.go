6876b3e0094744b53d04d1ec74862e41a4fda34b
afadesigns <afadesign.official@gmail.com>
2026-04-19 22:54:03 +0200

    feat: ZC1918 — detect `setopt HIST_SUBST_PATTERN` `:s` modifier semantics flip

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 914 Katas = 0.9.14
-const Version = "0.9.14"
+// 915 Katas = 0.9.15
+const Version = "0.9.15"
