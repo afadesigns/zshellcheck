43f0ff5d89a08ae3f1984e88d0496fdb86dad73d
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-20 00:56:04 +0200

    feat: ZC1937 — detect `tmux kill-server`/`screen -X quit` tearing down detached sessions (#958)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 933 Katas = 0.9.33
-const Version = "0.9.33"
+// 934 Katas = 0.9.34
+const Version = "0.9.34"
