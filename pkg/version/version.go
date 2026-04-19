45c4b78db76deb97ad7d407defa31443fbed1012
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-19 03:01:49 +0200

    feat: ZC1755 — flag `gcloud sql users ... --password PASS` (Cloud SQL pass in argv) (#774)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 751 Katas = 0.7.51
-const Version = "0.7.51"
+// 752 Katas = 0.7.52
+const Version = "0.7.52"
