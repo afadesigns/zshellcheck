40eb945764c011824899cacec72155e1872529ea
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-19 05:34:09 +0200

    feat: ZC1779 — error on az role assignment create --role Owner/Contributor/UAA (#798)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 775 Katas = 0.7.75
-const Version = "0.7.75"
+// 776 Katas = 0.7.76
+const Version = "0.7.76"
