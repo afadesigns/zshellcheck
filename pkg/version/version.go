a1425e501ba283e7c745d5c47d36ae7e0c403ad7
afadesigns <afadesign.official@gmail.com>
2026-04-19 21:46:32 +0200

    feat: ZC1905 — detect `ssh -g -L/-D` binding forwards on all interfaces

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 901 Katas = 0.9.1
-const Version = "0.9.1"
+// 902 Katas = 0.9.2
+const Version = "0.9.2"
