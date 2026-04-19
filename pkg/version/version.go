9f5ec95f02194df024ec7ffcefb421c560be8d78
afadesigns <afadesign.official@gmail.com>
2026-04-19 06:54:45 +0200

    feat: ZC1794 — error on cosign --insecure-ignore-tlog / --allow-insecure-registry

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 790 Katas = 0.7.90
-const Version = "0.7.90"
+// 791 Katas = 0.7.91
+const Version = "0.7.91"
