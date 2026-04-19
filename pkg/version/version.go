2fada882a3a07b0c6501b9633a2e3aec9849e063
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-19 07:44:17 +0200

    feat: ZC1803 — error on mysql --skip-ssl / psql sslmode=disable (#822)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 799 Katas = 0.7.99
-const Version = "0.7.99"
+// 800 Katas = 0.8.0
+const Version = "0.8.0"
