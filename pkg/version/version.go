99250d7a6cf57a4e6dfcb9ca8d1396167c52df4f
afadesigns <afadesign.official@gmail.com>
2026-04-19 02:01:07 +0200

    feat: ZC1747 — flag `npm/yarn/pnpm --registry http://...` (plaintext index, MITM)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 743 Katas = 0.7.43
-const Version = "0.7.43"
+// 744 Katas = 0.7.44
+const Version = "0.7.44"
