ebcc537e81a2760a41349cfc63470d6db4b70134
afadesigns <afadesign.official@gmail.com>
2026-04-19 21:25:35 +0200

    feat: ZC1901 — detect `setopt POSIX_BUILTINS` special-builtin scope flip

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 897 Katas = 0.8.97
-const Version = "0.8.97"
+// 898 Katas = 0.8.98
+const Version = "0.8.98"
