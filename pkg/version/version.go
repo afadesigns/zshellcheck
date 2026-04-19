f10adc843a04506acaca5ff9b8de348c54fd0290
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-19 16:38:10 +0200

    feat: ZC1851 — warn on `unsetopt FUNCTION_ARGZERO` hiding function $0 (#872)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 847 Katas = 0.8.47
-const Version = "0.8.47"
+// 848 Katas = 0.8.48
+const Version = "0.8.48"
