060bef255edae799bca8e535700394b7e45855e8
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-20 01:16:31 +0200

    feat: ZC1941 — detect `restic --insecure-no-password` unencrypted backup repo (#962)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 937 Katas = 0.9.37
-const Version = "0.9.37"
+// 938 Katas = 0.9.38
+const Version = "0.9.38"
