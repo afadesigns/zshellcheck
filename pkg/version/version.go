4a127d2b889faf89f0271b6cee427e61d392b3a2
afadesigns <afadesign.official@gmail.com>
2026-04-19 06:36:35 +0200

    feat: ZC1791 — error on curl --unix-socket to docker/containerd/crio/podman daemon

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 787 Katas = 0.7.87
-const Version = "0.7.87"
+// 788 Katas = 0.7.88
+const Version = "0.7.88"
