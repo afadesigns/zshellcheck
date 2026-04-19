567bea6ea257b703430e76fca3c1582166da17ed
afadesigns <afadesign.official@gmail.com>
2026-04-19 16:04:28 +0200

    feat: ZC1844 — warn on `logger -p local*.…` facility that rsyslog often drops

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 840 Katas = 0.8.40
-const Version = "0.8.40"
+// 841 Katas = 0.8.41
+const Version = "0.8.41"
