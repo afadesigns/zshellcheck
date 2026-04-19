41d0146a6dc8290161e120cfa767d637d942b348
afadesigns <afadesign.official@gmail.com>
2026-04-19 17:45:12 +0200

    feat: ZC1864 — error on `mount -o remount,exec|suid|dev` re-enabling hardened mount

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 860 Katas = 0.8.60
-const Version = "0.8.60"
+// 861 Katas = 0.8.61
+const Version = "0.8.61"
