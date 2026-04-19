042dce01e62a380af759f0e1c7260b7dc07785eb
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-19 06:49:13 +0200

    feat: ZC1793 — warn on kubectl certificate approve signing unreviewed CSR (#812)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 789 Katas = 0.7.89
-const Version = "0.7.89"
+// 790 Katas = 0.7.90
+const Version = "0.7.90"
