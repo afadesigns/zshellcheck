3f0651aec527562065ae7dd3fbf96bef2963b617
afadesigns <afadesign.official@gmail.com>
2026-04-19 07:13:00 +0200

    feat: ZC1797 — warn on ip link set <iface> down / ifdown locking out remote admin

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 793 Katas = 0.7.93
-const Version = "0.7.93"
+// 794 Katas = 0.7.94
+const Version = "0.7.94"
