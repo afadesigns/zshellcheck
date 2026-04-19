093eb85784407f016627a38afaffdbbdd9bccf98
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-19 10:49:13 +0200

    feat: ZC1833 — warn on `unsetopt WARN_CREATE_GLOBAL` hiding accidental globals (#854)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 829 Katas = 0.8.29
-const Version = "0.8.29"
+// 830 Katas = 0.8.30
+const Version = "0.8.30"
