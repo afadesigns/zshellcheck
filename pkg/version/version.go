99c61aba1c9babf2ef6e3d4f103c1f36c45c8848
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-19 17:16:27 +0200

    feat: ZC1858 — error on `ssh -c 3des-cbc|arcfour|…` forcing weak tunnel cipher (#879)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 854 Katas = 0.8.54
-const Version = "0.8.54"
+// 855 Katas = 0.8.55
+const Version = "0.8.55"
