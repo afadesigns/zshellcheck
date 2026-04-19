acb4f4208e64921098f7b1febd7fb1f2d726d37a
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-19 22:52:04 +0200

    feat: ZC1917 — detect `iw dev $IF scan` active WiFi probe from scripts (#938)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 913 Katas = 0.9.13
-const Version = "0.9.13"
+// 914 Katas = 0.9.14
+const Version = "0.9.14"
