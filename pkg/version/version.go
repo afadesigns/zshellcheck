70bbbf3e09e870b25f8b25333feef97d0f0e64ec
afadesigns <afadesign.official@gmail.com>
2026-04-19 18:56:40 +0200

    feat: ZC1875 — warn on `setopt RC_QUOTES` flipping `'/'` semantics in single quotes

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 871 Katas = 0.8.71
-const Version = "0.8.71"
+// 872 Katas = 0.8.72
+const Version = "0.8.72"
