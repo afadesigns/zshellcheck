355317084bb080cf95d1e990304b2f3cd6c08a70
afadesigns <afadesign.official@gmail.com>
2026-04-19 04:47:35 +0200

    feat: ZC1771 — warn on alias -g / alias -s in scripts

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 767 Katas = 0.7.67
-const Version = "0.7.67"
+// 768 Katas = 0.7.68
+const Version = "0.7.68"
