87376e07572290ac1500517e07bf0b7f1c9d05e6
afadesigns <afadesign.official@gmail.com>
2026-04-20 03:54:19 +0200

    feat: ZC1970 — detect `losetup -P`/`kpartx -a`/`partprobe` kernel partscan on untrusted image

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 966 Katas = 0.9.66
-const Version = "0.9.66"
+// 967 Katas = 0.9.67
+const Version = "0.9.67"
