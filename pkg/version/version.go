ef41a10b9515df657ee82dc6ea84637722cc8399
afadesigns <afadesign.official@gmail.com>
2026-04-20 04:31:39 +0200

    feat: ZC1977 — detect `setopt CHASE_DOTS` breaking blue/green `current` symlinks

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 973 Katas = 0.9.73
-const Version = "0.9.73"
+// 974 Katas = 0.9.74
+const Version = "0.9.74"
