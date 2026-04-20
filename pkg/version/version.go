06bf9b1a644e681f7d01871dff9487e9dff356c6
afadesigns <afadesign.official@gmail.com>
2026-04-20 05:38:27 +0200

    feat: ZC1988 — detect `nsupdate -y` TSIG key in argv

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 984 Katas = 0.9.84
-const Version = "0.9.84"
+// 985 Katas = 0.9.85
+const Version = "0.9.85"
