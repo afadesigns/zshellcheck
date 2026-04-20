aa0a0449bd5be1942f3dde49c2479b08c4066b0c
afadesigns <afadesign.official@gmail.com>
2026-04-20 06:48:34 +0200

    feat: ZC2000 — detect `kubectl taint nodes ...:NoExecute` mass pod eviction

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 996 Katas = 0.9.96
-const Version = "0.9.96"
+// 997 Katas = 0.9.97
+const Version = "0.9.97"
