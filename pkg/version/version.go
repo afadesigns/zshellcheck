aeee8538e0fd6f089858fb270a35319ebb45b220
afadesigns <afadesign.official@gmail.com>
2026-04-20 04:10:44 +0200

    feat: ZC1973 — detect `setopt POSIX_IDENTIFIERS` breaking Unicode parameter names

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 969 Katas = 0.9.69
-const Version = "0.9.69"
+// 970 Katas = 0.9.70
+const Version = "0.9.70"
