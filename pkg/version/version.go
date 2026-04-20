9f27f886cf72dda99c5710a486a7af81280871bc
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-20 05:50:48 +0200

    feat: ZC1990 — detect `openssl passwd -crypt`/`-1`/`-apr1` broken hash formats (#1012)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 986 Katas = 0.9.86
-const Version = "0.9.86"
+// 987 Katas = 0.9.87
+const Version = "0.9.87"
