fedd964d8445015798c1deae34e51ac788948350
afadesigns <afadesign.official@gmail.com>
2026-04-20 03:04:09 +0200

    feat: ZC1962 — detect `kustomize build --load-restrictor=None` path traversal

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 958 Katas = 0.9.58
-const Version = "0.9.58"
+// 959 Katas = 0.9.59
+const Version = "0.9.59"
