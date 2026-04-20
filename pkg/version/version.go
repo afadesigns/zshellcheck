da6159bf9424aeca451ef98c1e3bb0d816b2c526
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-20 05:19:31 +0200

    feat: ZC1985 — detect `setopt SH_FILE_EXPANSION` flipping Zsh expansion order (#1007)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 981 Katas = 0.9.81
-const Version = "0.9.81"
+// 982 Katas = 0.9.82
+const Version = "0.9.82"
