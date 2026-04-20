0bddd2b5101b95f467cffc059fee7c9871cf7a75
afadesigns <afadesign.official@gmail.com>
2026-04-20 03:35:40 +0200

    feat: ZC1967 — detect `setopt PROMPT_SUBST` prompt-substitution footgun

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 963 Katas = 0.9.63
-const Version = "0.9.63"
+// 964 Katas = 0.9.64
+const Version = "0.9.64"
