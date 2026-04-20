5ba8ed318016c0baa9ee400ec623c63c46b9d095
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-20 02:39:57 +0200

    feat: ZC1957 — detect `lvchange -an`/`vgchange -an` deactivating live volume (#978)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 953 Katas = 0.9.53
-const Version = "0.9.53"
+// 954 Katas = 0.9.54
+const Version = "0.9.54"
