9bb92dc3450e34de084f29a67248f01cdfdc4ee0
Andreas Fahl <151975154+afadesigns@users.noreply.github.com>
2026-04-20 06:15:21 +0200

    feat: ZC1995 — detect `unsetopt BG_NICE` bg jobs stealing interactive priority (#1017)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 991 Katas = 0.9.91
-const Version = "0.9.91"
+// 992 Katas = 0.9.92
+const Version = "0.9.92"
