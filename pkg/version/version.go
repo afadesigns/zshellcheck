a118ab2e17541871aaf7c5bd8cee399a0f767bfb
afadesigns <afadesign.official@gmail.com>
2026-04-20 06:34:34 +0200

    feat: ZC1998 — detect `tpm2_clear`/`tpm2 clear` wiping TPM storage hierarchy

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 // Version is the current version of ZShellCheck.
 // It is calculated based on the number of implemented Katas.
-// 994 Katas = 0.9.94
-const Version = "0.9.94"
+// 995 Katas = 0.9.95
+const Version = "0.9.95"
